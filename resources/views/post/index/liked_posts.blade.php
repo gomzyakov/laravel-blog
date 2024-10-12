09c0ce1126a02fb7ef9a89267226cb5552520e0d
gomzyakov <alexander.gomzyakov@gmail.com>
2024-10-12 18:52:17 +0500

    Update posts blades

---
+++ resources/views/post/index/liked_posts.blade.php
@@ -0,0 +1,27 @@
+@php
+  /** @var \App\Models\Post $likedPost */
+@endphp
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+        <h2>Liked posts</h2>
+    </div>
+</div>
+<div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+    @foreach($likedPosts as $likedPost)
+        <div class="col">
+            <div class="card h-100">
+                <img src="{{ $likedPost->preview_image }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a class="card-title" href="{{ route('post.show', $likedPost->id) }}">
+                        {{ $likedPost->title }}
+                    </a>
+                    <p class="card-text">
+                        {{ $likedPost->shortBody() }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+
