5e9825b3e0aa4e683bab4aa497c9ab79fd9d8568
gomzyakov <alexander.gomzyakov@gmail.com>
2024-10-13 19:42:52 +0500

    Posts list

--- resources/views/layouts/wrapper-admin/sidebar.blade.php
+++ resources/views/layouts/wrapper-admin/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary h-100">
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary d-flex flex-column flex-shrink-0">
     <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu"
          aria-labelledby="sidebarMenuLabel">
         <div class="offcanvas-header">
@@ -52,48 +52,48 @@
                 </li>
             </ul>
 
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-                <span>Saved reports</span>
-                <a class="link-secondary" href="#" aria-label="Add a new report">
-                    <svg class="bi">
-                        <use xlink:href="#plus-circle"/>
-                    </svg>
-                </a>
-            </h6>
-            <ul class="nav flex-column mb-auto">
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
-                        <svg class="bi">
-                            <use xlink:href="#file-earmark-text"/>
-                        </svg>
-                        Current month
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
-                        <svg class="bi">
-                            <use xlink:href="#file-earmark-text"/>
-                        </svg>
-                        Last quarter
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
-                        <svg class="bi">
-                            <use xlink:href="#file-earmark-text"/>
-                        </svg>
-                        Social engagement
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
-                        <svg class="bi">
-                            <use xlink:href="#file-earmark-text"/>
-                        </svg>
-                        Year-end sale
-                    </a>
-                </li>
-            </ul>
+{{--            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">--}}
+{{--                <span>Saved reports</span>--}}
+{{--                <a class="link-secondary" href="#" aria-label="Add a new report">--}}
+{{--                    <svg class="bi">--}}
+{{--                        <use xlink:href="#plus-circle"/>--}}
+{{--                    </svg>--}}
+{{--                </a>--}}
+{{--            </h6>--}}
+{{--            <ul class="nav flex-column mb-auto">--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link d-flex align-items-center gap-2" href="#">--}}
+{{--                        <svg class="bi">--}}
+{{--                            <use xlink:href="#file-earmark-text"/>--}}
+{{--                        </svg>--}}
+{{--                        Current month--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link d-flex align-items-center gap-2" href="#">--}}
+{{--                        <svg class="bi">--}}
+{{--                            <use xlink:href="#file-earmark-text"/>--}}
+{{--                        </svg>--}}
+{{--                        Last quarter--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link d-flex align-items-center gap-2" href="#">--}}
+{{--                        <svg class="bi">--}}
+{{--                            <use xlink:href="#file-earmark-text"/>--}}
+{{--                        </svg>--}}
+{{--                        Social engagement--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link d-flex align-items-center gap-2" href="#">--}}
+{{--                        <svg class="bi">--}}
+{{--                            <use xlink:href="#file-earmark-text"/>--}}
+{{--                        </svg>--}}
+{{--                        Year-end sale--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
 
             <hr class="my-3">
 
